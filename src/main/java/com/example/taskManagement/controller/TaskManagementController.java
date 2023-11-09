6c5f5fc677c15b4f32fa46ee6f6c92be26359185
Mahendar Kumawat R <90560612+MahendarKumawatR@users.noreply.github.com>
2023-11-09 12:44:09 +0530

    Minor changes

--- src/main/java/com/example/taskManagement/controller/TaskManagementController.java
+++ src/main/java/com/example/taskManagement/controller/TaskManagementController.java
@@ -20,50 +20,52 @@ public class TaskManagementController {
         this.taskManagementService = taskManagementService;
     }
 
-    @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping("/Tasks")
-    public ResponseEntity<Void> createTask(@RequestBody Task task){
-        Task taskCreated = taskManagementService.createTask(task);
-        URI uri = URI.create("/Tasks/" + taskCreated.getId());
-        return ResponseEntity.created(uri).build();
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/Tasks/nextNDays/{nextNDays}")
+    public List<Task> getTask1(@PathVariable("nextNDays") Integer nextNDays){
+        return taskManagementService.findAllTasksForNextNDays(nextNDays);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/Tasks/allCriticalAndHighPriorityTasks")
+    public List<Task> getAllCriticalAndHighPriorityTasks(){
+        return taskManagementService.findAllCriticalAndHighPriorityTasks();
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/Tasks/allTasksByLabelNameForToday/{labelName}")
+    public List<Task> getAllTasksByLabelNameForToday(@PathVariable("labelName") String labelName){
+        return taskManagementService.findAllTasksByLabelNameForToday(labelName);
     }
 
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/Tasks/{taskId}")
     public ResponseEntity<Task> getTask(@PathVariable("taskId") String taskId){
-        Task task = taskManagementService.getTask(taskId);
+        Task task = taskManagementService.findOne(taskId);
         return new ResponseEntity<>(task, HttpStatus.OK);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/Tasks")
+    public ResponseEntity<Void> createTask(@RequestBody Task task){
+        Task taskCreated = taskManagementService.save(task);
+        URI uri = URI.create("/Tasks/" + taskCreated.getId());
+        return ResponseEntity.created(uri).build();
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PutMapping("/Tasks/{taskId}")
     public ResponseEntity<Void> updateTask(@PathVariable("taskId") String taskId, @RequestBody Task task){
-        Task task1 = taskManagementService.updateTask(taskId, task);
+        Task task1 = taskManagementService.update(taskId, task);
         return ResponseEntity.noContent().build();
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/Tasks/{taskId}")
     public ResponseEntity<Void> deleteTask(@PathVariable("taskId") String taskId){
-        taskManagementService.deleteTask(taskId);
+        taskManagementService.delete(taskId);
         return ResponseEntity.noContent().build();
     }
 
-    @ResponseStatus(HttpStatus.OK)
-    @GetMapping("/Tasks/nextNDays/{nextNDays}")
-    public List<Task> getTask1(@PathVariable("nextNDays") Integer nextNDays){
-        return taskManagementService.getAllTasksForNextNDays(nextNDays);
-    }
 
-    @ResponseStatus(HttpStatus.OK)
-    @GetMapping("/Tasks/allCriticalAndHighPriorityTasks")
-    public List<Task> getAllCriticalAndHighPriorityTasks(){
-        return taskManagementService.getAllCriticalAndHighPriorityTasks();
-    }
-
-    @ResponseStatus(HttpStatus.OK)
-    @GetMapping("/Tasks/allTasksByLabelNameForToday/{labelName}")
-    public List<Task> getAllTasksByLabelNameForToday(@PathVariable("labelName") String labelName){
-        return taskManagementService.getAllTasksByLabelNameForToday(labelName);
-    }
 }
--- src/main/java/com/example/taskManagement/service/TaskManagementService.java
+++ src/main/java/com/example/taskManagement/service/TaskManagementService.java
@@ -22,7 +22,32 @@ public class TaskManagementService {
         this.taskManagementRepository = taskManagementRepository;
     }
 
-    public Task createTask(Task task) {
+     public List<Task> findAllCriticalAndHighPriorityTasks() {
+        List<Priority> priorityList = new ArrayList<>();
+        priorityList.add(Priority.CRITICAL);
+        priorityList.add(Priority.HIGH);
+        return taskManagementRepository.getCriticalAndHighPriorityTasks(priorityList);
+    }
+
+    public List<Task> findAllTasksForNextNDays(Integer nextNDays) {
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        LocalDateTime afterNDaysDateTime = currentDateTime.plusDays(nextNDays);
+        LocalDateTime futureDateTime = afterNDaysDateTime.with(LocalTime.MAX);
+        return taskManagementRepository.findByTaskStartTimeBetween(currentDateTime, futureDateTime);
+    }
+
+    public List<Task> findAllTasksByLabelNameForToday(String labelName) {
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        LocalDateTime startOfDay = currentDateTime.with(LocalTime.MIN);
+        LocalDateTime endOfDay = currentDateTime.with(LocalTime.MAX);
+        return taskManagementRepository.findByLabelLabelNameAndTaskStartTimeBetween(labelName, startOfDay, endOfDay);
+    }
+
+    public Task findOne(String taskId) {
+        return taskManagementRepository.findById(taskId).orElseThrow(() ->new RuntimeException("Task with this taskId not present"));
+    }
+
+    public Task save(Task task) {
         System.out.println("Task Entity before persisting: "+ task);
         String uuid = UUID.randomUUID().toString();
         task.setId(uuid);
@@ -31,40 +56,15 @@ public class TaskManagementService {
         return presistedTask;
     }
 
-    public Task getTask(String taskId) {
-        return taskManagementRepository.findById(taskId).orElseThrow(() ->new RuntimeException("Task with this taskId not present"));
-    }
-
-    public Task updateTask(String taskId, Task task) {
+    public Task update(String taskId, Task task) {
         task.setId(taskId);
         taskManagementRepository.findById(taskId).orElseThrow(() -> new RuntimeException("Task with this taskId not present"));
         Task savedTask = taskManagementRepository.save(task);
         return savedTask;
     }
 
-    public void deleteTask(String taskId) {
+    public void delete(String taskId) {
         Task task = taskManagementRepository.findById(taskId).orElseThrow(() -> new RuntimeException("Task with this taskId not present"));
         taskManagementRepository.delete(task);
-    }
-
-    public List<Task> getAllCriticalAndHighPriorityTasks() {
-        List<Priority> priorityList = new ArrayList<>();
-        priorityList.add(Priority.CRITICAL);
-        priorityList.add(Priority.HIGH);
-        return taskManagementRepository.getCriticalAndHighPriorityTasks(priorityList);
-    }
-
-    public List<Task> getAllTasksForNextNDays(Integer nextNDays) {
-        LocalDateTime currentDateTime = LocalDateTime.now();
-        LocalDateTime afterNDaysDateTime = currentDateTime.plusDays(nextNDays);
-        LocalDateTime futureDateTime = afterNDaysDateTime.with(LocalTime.MAX);
-        return taskManagementRepository.findByTaskStartTimeBetween(currentDateTime, futureDateTime);
-    }
-
-    public List<Task> getAllTasksByLabelNameForToday(String labelName) {
-        LocalDateTime currentDateTime = LocalDateTime.now();
-        LocalDateTime startOfDay = currentDateTime.with(LocalTime.MIN);
-        LocalDateTime endOfDay = currentDateTime.with(LocalTime.MAX);
-        return taskManagementRepository.findByLabelLabelNameAndTaskStartTimeBetween(labelName, startOfDay, endOfDay);
     }
 }
